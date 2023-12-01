e69a34c15eeea15039853db8d6210ba99ca729d5
MikWink <116279962+MikWink@users.noreply.github.com>
2023-12-01 18:41:32 +0100

    Part one done

--- day1.py
+++ day1.py
@@ -1,9 +1,21 @@
 data = open("day1_data.txt", "r")
 numbers_in_string = []
+sum = 0
+string_number = ""
+number = 0
 for line in data:
     print(line)
     for char in line:
         if char.isdigit():
-            numbers_in_string.insert(char)
+            numbers_in_string.append(char)
+    if len(numbers_in_string) == 1:
+        string_number = "" + numbers_in_string[0] + numbers_in_string[0]
+        sum += int(string_number)
+    elif len(numbers_in_string) >> 1:
+        string_number = "" + numbers_in_string[0] + numbers_in_string[len(numbers_in_string) - 1]
+        sum += int(string_number)
+    else:
+        print("No Numbers.")
 
-    print(numbers_in_string)
+    numbers_in_string.clear()
+print(sum)
